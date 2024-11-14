f57a97fe2936acad413ef6e36901041accaadf4c
Jay Lee <jaeho.lee@snu.ac.kr>
2024-11-14 16:39:20 +0900

    :sparkles: Add cloudflare warp

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -26,6 +26,7 @@
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   networking.networkmanager.enable = true;
+  services.cloudflare-warp.enable = true;
 
   nix.settings.experimental-features = [
     "nix-command"
