e8ae0dbbdc1a043dceccb53975c02457e585b85b
Jay Lee <jaeho.lee@snu.ac.kr>
2024-11-25 23:46:33 +0900

    :lipstick: Add bat tokyonight day theme

--- homeManagerModules/terminal.nix
+++ homeManagerModules/terminal.nix
@@ -85,7 +85,23 @@
       eza.enable = true;
 
       zoxide.enable = true;
-      bat.enable = true;
+      bat = {
+        enable = true;
+        config = {
+          theme = "tokyonight_day";
+        };
+        themes = {
+          tokyonight_day = {
+            src = pkgs.fetchFromGitHub {
+              owner = "folke";
+              repo = "tokyonight.nvim";
+              rev = "c2725eb6d086c8c9624456d734bd365194660017";
+              sha256 = "sha256-vKXlFHzga9DihzDn+v+j3pMNDfvhYHcCT8GpPs0Uxgg=";
+            };
+            file = "extras/sublime/tokyonight_day.tmTheme";
+          };
+        };
+      };
       fd.enable = true;
       fzf = {
         enable = true;
