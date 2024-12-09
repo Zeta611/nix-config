a209307c6c49f996a544a6fa232201a88ef251b0
Jay Lee <jaeho.lee@snu.ac.kr>
2024-12-10 03:00:00 +0900

    :bug: Make zathura use nvim from PATH

--- homeManagerModules/nvim/lua/plugins/vimtex.lua
+++ homeManagerModules/nvim/lua/plugins/vimtex.lua
@@ -6,7 +6,9 @@ return {
 		if hostname == "jay-macbook-pro" then
 			vim.g.vimtex_view_method = "skim"
 		elseif hostname == "jay-nixos" then
-			vim.g.vimtex_view_method = "zathura"
+			vim.g.vimtex_view_general_viewer = "zathura"
+			vim.g.vimtex_view_general_options =
+				[[ -x "nvim --headless -c \"VimtexInverseSearch %{line}:%{column} '%{input}'\"" --synctex-forward @line:@col:'@tex' '@pdf' ]]
 		end
 	end,
 }
