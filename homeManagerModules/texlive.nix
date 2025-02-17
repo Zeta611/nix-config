204b1fe0c6082c1e6bb6a7670bd0116f435e54bf
Jay Lee <jaeho.lee@snu.ac.kr>
2025-02-17 21:43:11 +0900

    :sparkles: Add texmf directory

--- homeManagerModules/texlive.nix
+++ homeManagerModules/texlive.nix
@@ -10,11 +10,47 @@
     texlive.enable = lib.mkEnableOption "enable texlive";
   };
 
-  config = lib.mkIf config.texlive.enable {
+  config = let
+    ebproofx = pkgs.stdenvNoCC.mkDerivation {
+      name = "ebproofx";
+      src = ./texmf;
+      installPhase = "cp -r $src $out";
+      passthru.tlType = "run";
+    };
+    latex-report-classes = pkgs.stdenvNoCC.mkDerivation {
+      name = "latex-report-classes";
+      src = ./texmf;
+      installPhase = "cp -r $src $out";
+      passthru.tlType = "run";
+    };
+    simplesnt = pkgs.stdenvNoCC.mkDerivation {
+      name = "simplesnt";
+      src = ./texmf;
+      installPhase = "cp -r $src $out";
+      passthru.tlType = "run";
+    };
+    snu-ece-bsc-thesis = pkgs.stdenvNoCC.mkDerivation {
+      name = "snu-ece-bsc-thesis";
+      src = ./texmf;
+      installPhase = "cp -r $src $out";
+      passthru.tlType = "run";
+    };
+    zdoc = pkgs.stdenvNoCC.mkDerivation {
+      name = "zdoc";
+      src = ./texmf;
+      installPhase = "cp -r $src $out";
+      passthru.tlType = "run";
+    };
+    custom-texmf = {
+      pkgs = [ simplesnt ];
+    };
+  in
+  lib.mkIf config.texlive.enable {
     programs.texlive = {
       enable = true;
       extraPackages = tpkgs: {
         inherit (tpkgs) scheme-full;
+        inherit custom-texmf;
         pkgFilter = pkg: pkg.tlType == "run" || pkg.tlType == "bin" || pkg.tlType == "doc" || pkg.tlType == "source";
       };
     };
