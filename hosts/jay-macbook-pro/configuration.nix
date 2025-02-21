7adcda7a9c9006b114a0d0e2e52a54e7336aea5f
Jay Lee <jaeho.lee@snu.ac.kr>
2025-02-22 02:21:55 +0900

    :alien: Upgrade flakes and remove deprecated option

--- hosts/jay-macbook-pro/configuration.nix
+++ hosts/jay-macbook-pro/configuration.nix
@@ -14,9 +14,6 @@
     # inputs.wezterm.packages.${pkgs.system}.default
   ];
 
-  # Auto upgrade nix package and the daemon service.
-  services.nix-daemon.enable = true;
-
   nix.settings.experimental-features = [
     "nix-command"
     "flakes"
