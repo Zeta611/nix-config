2ff1cbe67d591fbe0e947bd324a1aafcc87ec3d0
Jay Lee <jaeho.lee@snu.ac.kr>
2025-03-22 16:17:56 +0900

    :wrench: Update aerospace top margin and make the config a symbolic link

--- homeManagerModules/aerospace.nix
+++ homeManagerModules/aerospace.nix
@@ -14,6 +14,6 @@
     home.packages = with pkgs; [
       aerospace
     ];
-    xdg.configFile."aerospace/aerospace.toml".source = ./aerospace.toml;
+    xdg.configFile."aerospace/aerospace.toml".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix/homeManagerModules/aerospace.toml";
   };
 }
