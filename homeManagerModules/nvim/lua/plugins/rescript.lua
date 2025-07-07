42e246b02d6fd5fbb24dafc464cfb2d4a8afa703
Jay Lee <jaeho.lee@snu.ac.kr>
2025-07-07 20:00:14 +0900

    :sparkles: Add rescriptls settings

--- homeManagerModules/nvim/lua/plugins/rescript.lua
+++ homeManagerModules/nvim/lua/plugins/rescript.lua
@@ -1,4 +1,13 @@
 return {
-	"rescript-lang/vim-rescript",
-	ft = "rescript",
+	{ "rescript-lang/vim-rescript", ft = "rescript" },
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			servers = {
+				rescriptls = {
+					mason = false,
+				},
+			},
+		},
+	},
 }
